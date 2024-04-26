b0fac9ef3fc66702346132ba6936f8e4d3326c84
Omar-Mega-Byte <113197407+Omar-Mega-Byte@users.noreply.github.com>
2024-04-26 21:18:29 +0300

    Add files via upload

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -1,12 +1,11 @@
 <?php
 require_once '../Models/User.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if(isset($_POST['username'], $_POST['password'], $_POST['email'])) {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+    if(isset($_POST['email']) && isset($_POST['password'])) {
         $email = $_POST['email'];
+        $password = $_POST['password'];
         $newUser = new User();
-        $newUser->Register($username,$email,$password);
+        $newUser->Login($email,$password);
     } else {
         echo "All fields are required.";
         exit();
--- Models/User.php
+++ Models/User.php
@@ -7,7 +7,6 @@ class User {
     private $password;
     public function __construct() {
     }
-
     public function getUserName() {
         return $this->username;
     }
@@ -15,11 +14,9 @@ class User {
     public function getPassword() {
         return $this->password;
     }
-
     public function setUserName($username) {
         $this->username = $username;
     }
-
     public function setPassword($password) {
         $this->password = $password;
     }
@@ -33,5 +30,20 @@ class User {
         require('../Views/Home.html');
         exit();
 }
+    public function Login($email,$password){
+        $conn = new mysqli('localhost', 'root', '', 'projectsw');
+        if ($conn->connect_error) {
+            die('Connection error: ' . $conn->connect_error);
+        }
+        $sql = "SELECT * FROM `login` WHERE email ='$email' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
+        if ($row = mysqli_fetch_assoc($result)) {
+            require('../Views/Home.html');
+            exit();
+        } else {
+            require('../Views/Error.html');
+            exit();
+        }
+    }
 }
 ?>
